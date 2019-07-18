6c9ec35836171acaf9b6712d2c2d8124a243c2e3
Dennis Dwarte <dennisd1@icloud.com>
2019-07-18 22:11:00 +1000

    Add MVC Model

---
+++ TodoDMD/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TodoDMD
+//
+//  Created by Dennis Dwarte on 18/7/19.
+//  Copyright © 2019 Dennis Dwarte. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
